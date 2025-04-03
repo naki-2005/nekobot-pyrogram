6a2ff22c9568ab59972e0d3a380f485c719a0cf2
Naki <alicia.neko@mail.ru>
2025-04-03 10:45:41 -0400

    Update videotools.py

--- command/videotools.py
+++ command/videotools.py
@@ -96,7 +96,7 @@ async def cancelar_tarea(admin_users, client, task_id, chat_id, message, allowed
         await client.send_message(chat_id=chat_id, text=f"⚠️ No se encontró la tarea con ID `{task_id}`.", protect_content=protect_content)
 
 # Listar tareas
-async def listar_tareas(client, chat_id, user_id, message, admin_users):
+async def listar_tareas(client, chat_id, user_id, message):
     user_id_requesting = user_id
     protect_content = user_id_requesting not in admin_users
 
