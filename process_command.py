35186596b481741766604e4c711c7f8bd4851161
Naki <alicia.neko@mail.ru>
2025-06-19 15:21:20 -0400

    Update process_command.py

--- process_command.py
+++ process_command.py
@@ -9,7 +9,7 @@ from command.htools import nh_combined_operation, cambiar_default_selection
 from command.admintools import add_user, remove_user, add_chat, remove_chat, ban_user, deban_user, handle_start
 from command.imgtools import create_imgchest_post
 from command.webtools import handle_scan, handle_multiscan, summarize_lines
-from command.mailtools import send_mail, set_mail, verify_mail, set_mail_limit, set_mail_delay
+from command.mailtools import send_mail, set_mail, verify_mail, set_mail_limit_mb, set_mail_limit_kb, set_mail_delay
 from command.videotools import update_video_settings, compress_video, cancelar_tarea, listar_tareas, cambiar_miniatura
 from command.filetools import handle_compress, rename, set_size, caption
 from command.telegramtools import get_file_id, send_file_by_id
@@ -151,7 +151,7 @@ async def process_command(client: Client, message: Message, active_cmd: str, adm
         return
 
     
-    elif text.startswith(("/setmail", "/sendmail", "/verify", "/setmb", "/setdelay")):
+    elif text.startswith(("/setmail", "/sendmail", "/verify", "/setmb","/setkb", "/setdelay")):
         if cmd("mailtools", user_id in admin_users, user_id in vip_users):
             if text.startswith("/setmail"):
                 await asyncio.create_task(set_mail(client, message))
@@ -159,7 +159,10 @@ async def process_command(client: Client, message: Message, active_cmd: str, adm
                 await asyncio.create_task(send_mail(client, message))
 
             elif text.startswith("/setmb"):
-                await asyncio.create_task(set_mail_limit(client, message))
+                await asyncio.create_task(set_mail_limit_mb(client, message))
+
+            elif text.startswith("/setkb"):
+                await asyncio.create_task(set_mail_limit_kb(client, message))
 
             elif text.startswith("/setdelay"):
                 await asyncio.create_task(set_mail_delay(client, message))
