af34019a2936c110771fdddb69610a0c92b897e7
Naki <alicia.neko@mail.ru>
2025-08-28 16:48:59 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 mkdir -p server
 python3 -m http.server -d server &
-if [[ -n "$TOKEN" && -n "$SESSION_STRING" ]]; then
+
+# Verificar que solo uno esté definido
+if [ -n "$TOKEN" ] && [ -n "$SESSION_STRING" ]; then
   echo "❌ No puedes usar TOKEN y SESSION_STRING al mismo tiempo."
   exit 1
-elif [[ -z "$TOKEN" && -z "$SESSION_STRING" ]]; then
+elif [ -z "$TOKEN" ] && [ -z "$SESSION_STRING" ]; then
   echo "❌ Debes definir TOKEN o SESSION_STRING en el entorno."
   exit 1
 fi
-if [[ -n "$TOKEN" ]]; then
+
+# Ejecutar según el caso
+if [ -n "$TOKEN" ]; then
   python3 neko.py -a "$API_ID" -H "$API_HASH" -t "$TOKEN"
 else
   python3 neko.py -a "$API_ID" -H "$API_HASH" -ss "$SESSION_STRING"
