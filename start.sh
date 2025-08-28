106c5a85aa37455f2d81aae421e616f0d4d3c30d
Naki <alicia.neko@mail.ru>
2025-08-28 16:26:18 -0400

    Una Pequeña Prueba

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-mkdir server
-python3 -m http.server -d server &
-python3 neko.py
 #!/bin/bash
+
+mkdir -p server
+python3 -m http.server -d server &
+python3 neko.py -a "$API_ID" -h "$API_HASH" -t "$TOKEN"
