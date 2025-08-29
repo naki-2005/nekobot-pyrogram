1ca2e6ea250749372c0d8042424ab52f52786a98
Naki <alicia.neko@mail.ru>
2025-08-29 08:48:17 -0400

    Update cmd_list.py

--- cmd_list.py
+++ cmd_list.py
@@ -13,13 +13,15 @@ async def lista_cmd(app):
         BotCommand("convert", "Convierte un video"),
         BotCommand("calidad", "Ajusta la calidad de /convert"),
         BotCommand("setmail", "Configure su correo para usar /send"),
+        BotCommand("verify", "Verifica tu correo con un código"),
         BotCommand("setdelay", "Configure el delay del correo"),
         BotCommand("setmb", "Configure el peso de las partes"),
         BotCommand("sendmail", "Envía un archivo dividido en 7z a su correo"),
         BotCommand("sendmailb", "Envía un archivo dividido en bytes a su correo"),
         BotCommand("upfile", "Sube un archivo al servidor"),
+        BotCommand("listfiles", "Mostrar lista de archivos en el servidor"),
+        BotCommand("sendfile", "Subir un archivo almacenado en el servidor"),
         BotCommand("clearfiles", "Borra los archivos en el servidor"),
-        BotCommand("verify", "Verifica tu correo con un código"),
         BotCommand("setfile", "Define su preferencia de descarga de Hentai"),
         BotCommand("nh", "Descarga un manga hentai de Nhentai"),
         BotCommand("3h", "Descarga un manga hentai de 3Hentai"),
@@ -31,7 +33,7 @@ async def lista_cmd(app):
         BotCommand("resumetxtcodes", "Extrae codigos Hentai y los envia en txt"),
         BotCommand("codesplit", "Divide la cantidas de códigos en un txt"),
         BotCommand("imgchest", "Publica una imagen en Imgchest"),
-        BotCommand("access", "Obten acceso al bot mediante una contraseña"),
         BotCommand("settings", "(owner) configura el bot"),
         BotCommand("edituser", "(admin) controla el acceso al bot")
     ])
+    
