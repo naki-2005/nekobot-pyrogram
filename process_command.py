ef1d2621f83cd60ef91c108fcd91892ed3da04cf
Naki <alicia.neko@mail.ru>
2025-09-07 13:55:44 -0400

    Update process_command.py

--- process_command.py
+++ process_command.py
@@ -424,6 +424,7 @@ async def process_command(client, message, user_id, username, chat_id, int_lvl):
         if args[0] == "cookies":
             from command.yt_tools import handle_get_cookies
             await handle_get_cookies(client, message)
+            return
         if args[0] == "copy" and len(args) >= 2:
             from command.db.db import descargar_web_config, descargar_bot_config, subir_bot_config
             bot_id = args[1]
